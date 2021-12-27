9b688a65e1169f6230bdbb517dc3a25b02ce57f0
tedison <ktjiss0930@gmail.com>
2021-12-27 23:08:04 +0900

    chapter2 액터 설계

--- Source/ArenaBattle/Fountain.cpp
+++ Source/ArenaBattle/Fountain.cpp
@@ -6,9 +6,40 @@
 // Sets default values
 AFountain::AFountain()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
+	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
+	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
+	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+
+	RootComponent = Body;
+	Water->SetupAttachment(Body);
+	Light->SetupAttachment(Body);
+	Splash->SetupAttachment(Body);
+
+	Water->SetRelativeLocation(FVector(0.0f, 0.0f, 135.0f));
+	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01"));
+	if (SM_BODY.Succeeded())
+	{
+		Body->SetStaticMesh(SM_BODY.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02.SM_Plains_Fountain_02"));
+	if (SM_WATER.Succeeded())
+	{
+		Water->SetStaticMesh(SM_WATER.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem>PS_SPALSH(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01"));
+	if (PS_SPALSH.Succeeded())
+	{
+		Splash->SetTemplate(PS_SPALSH.Object);
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/ArenaBattle/Fountain.h
+++ Source/ArenaBattle/Fountain.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Fountain.generated.h"
 
@@ -23,4 +23,18 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Body;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Water;
+
+	UPROPERTY(VisibleAnywhere)
+	UPointLightComponent* Light;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent* Splash;
+
+	UPROPERTY(EditAnywhere, Category=ID)
+	int32 ID;
 };
