af0d66665d918ee21f2a6f3dd71ed95229e788e9
tedison <ktjiss0930@gmail.com>
2022-01-01 23:26:22 +0900

    chapter4 게임플레이 프레임워크

---
+++ Source/ArenaBattle/ABGameMode.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABGameMode.h"
+#include "ABPawn.h"
+#include "ABPlayercontroller.h"
+
+AABGameMode::AABGameMode()
+{
+	DefaultPawnClass = AABPawn::StaticClass();
+	PlayerControllerClass = AABPlayerController::StaticClass();
+
+	static ConstructorHelpers::FClassFinder<APawn> BP_PAWN_C(TEXT("/Game/ThirdPersonBP/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter_C"));
+	if (BP_PAWN_C.Succeeded())
+	{
+		DefaultPawnClass = BP_PAWN_C.Class;
+	}
+}
+
+void AABGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	ABLOG(Warning, TEXT("PostLogin Begin"));
+	Super::PostLogin(NewPlayer);
+	ABLOG(Warning, TEXT("PostLogin End"));
+}
